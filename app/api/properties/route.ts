6cfa01c791ca8bba13eed15cf810e42e8e328602
Quang Le <quanglm>
2024-07-03 11:09:58 +0700

    upload to cloudinary

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -4,6 +4,7 @@ import { request } from 'http';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/utils/authOptions';
 import { getSessionUser } from '@/utils/getSessionUser';
+import cloudinary from '@/config/cloudinary';
 
 //GET /api/properties
 export const GET = async (request: any) => {
@@ -59,8 +60,34 @@ export const POST = async (request: any) => {
 				email: formData.get('seller_info.email'),
 			},
 			owner: sessionUser.userId,
-			// images,
+			images,
 		};
+
+		//Upload images to Cloudinary
+		const imageUploadPromises = [];
+		for (const image of images) {
+			const imageBuffer = await image.arrayBuffer();
+			const imageArray = Array.from(new Uint8Array(imageBuffer));
+			const imageData = Buffer.from(imageArray);
+
+			//Convert the image data to base64
+			const imageBase64 = imageData.toString('base64');
+
+			//Make request to upload to Cloudinary
+			const result = await cloudinary.uploader.upload(
+				`data:image/png;base64,${imageBase64}`,
+				{
+					folder: 'propertypulse',
+				}
+			);
+
+			imageUploadPromises.push(result.secure_url);
+
+			//Wait for all images to upload
+			const uploadedImages = await Promise.all(imageUploadPromises);
+			//Add uploaded images to the propertyData object
+			propertyData.images = uploadedImages;
+		}
 
 		const newProperty = new Property(propertyData);
 		await newProperty.save();
